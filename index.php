4ea2e851347da0fd8247c1cd6ac8e57634c880be
LeoG <103144119+LeoG1904@users.noreply.github.com>
2024-04-01 17:27:15 +0200

    ajout des liens dans le index

--- index.php
+++ index.php
@@ -1,7 +1,51 @@
 <?php
+
+include_once 'control/Controller.php';
+include_once 'control/Presenter.php';
+
 include_once 'gui/Vue.php';
 include_once 'gui/VueConnexion.php';
 include_once 'gui/VueMenu.php';
 include_once 'gui/VuePlat.php';
 
+use control\{Controller, Presenter};
 use gui\{VueConnexion, VueMenu, VuePlat, Vue};
+
+$controller = new Controller();
+$presenter = new Presenter();
+
+// chemin de l'URL demandée au navigateur
+// (p.ex. /annonces/index.php)
+$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+// définition d'une session d'une heure
+ini_set('session.gc_maxlifetime', 3600);
+session_set_cookie_params(3600);
+session_start();
+
+
+if ('/restaurant/' == $uri || '/restaurant/index.php' == $uri){
+	//page de connexion
+	header('Status: 404 Not Found');
+	echo '<html><body><h1>Page connexion</h1></body></html>';
+}
+elseif ('/restaurant/index.php/commande' == $uri){
+	//page de commande
+	header('Status: 404 Not Found');
+	echo '<html><body><h1>Page commande</h1></body></html>';
+}
+elseif ('/restaurant/index.php/menu' == $uri){
+	//page de menu
+	header('Status: 404 Not Found');
+	echo '<html><body><h1>Page menu</h1></body></html>';
+}
+elseif ('/restaurant/index.php/plat' == $uri){
+	//page de plat
+	header('Status: 404 Not Found');
+	echo '<html><body><h1>Page plat</h1></body></html>';
+}
+else {
+	header('Status: 404 Not Found');
+	echo '<html><body><h1>Page introuvable</h1></body></html>';
+}
